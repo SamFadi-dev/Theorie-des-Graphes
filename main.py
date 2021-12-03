248d9d4d20d87adf733330408dff658677abda05
Pouria <pouria.hakhamanesh25@gmail.com>
2021-12-03 19:51:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -67,13 +67,13 @@ while ans:
     #about
     elif(ans == 4):
     print("\n")
-    print("University of Liege (2021-2022)")
-    print("This program is created by 2 computer science students.")
-    print("SALEHIKATOZI Pouria & HAMIE Sam")
-    print("Dinic's algorithm or Dinitz's algorithm is a strongly polynomial algorithm for computing the maximum flow in a flow network, conceived in 1970 by Israeli (formerly Soviet) computer scientist Yefim (Chaim) A. Dinitz.")
-    print("For more information about Dinic (Dinitz) algorithms:")
-    print("https://en.wikipedia.org/wiki/Dinic%27s_algorithm")
-    print("ⒸAll copyrights of this program are reserved for these 2 students.")
+    print("University of Liege (2021-2022) \n")
+    print("This program is created by 2 computer science students. \n")
+    print("SALEHIKATOZI Pouria & HAMIE Sam \n")
+    print("Dinic's algorithm or Dinitz's algorithm is a strongly polynomial algorithm for computing the maximum flow in a flow network, conceived in 1970 by Israeli (formerly Soviet) computer scientist Yefim (Chaim) A. Dinitz.\n")
+    print("For more information about Dinic (Dinitz) algorithms: \n")
+    print("https://en.wikipedia.org/wiki/Dinic%27s_algorithm \n")
+    print("ⒸAll copyrights of this program are reserved for these 2 students. \n")
 
     #Quitter le programme
     elif(ans == 5):
